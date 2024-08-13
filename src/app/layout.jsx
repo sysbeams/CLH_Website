faf1b479ef1513f831f2ef9e6c77666a2b93b55e
Mazeedah Hamzat <mazeedahhamzat@gmail.com>
2024-08-13 22:19:41 +0100

    sitemap

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="5NodRWHf3zcQrdQ9ovWELJ67E3K_f_eWtkWWvduT34o" />
+      </head>
       <body className={quickSand.className}>{children}</body>
     </html>
   );
