8af8246f3af1a0d14200b60379768a9b95c4f098
Mazeedah Hamzat <mazeedahhamzat@gmail.com>
2024-08-13 22:21:56 +0100

    sitemap

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,14 +6,12 @@ const quickSand = Quicksand({ subsets: ["latin"], weights: [400, 500, 600, 700] 
 export const metadata = {
   title: "CLH",
   description: "Code Learners Hub",
+  googleSiteVerification: "5NodRWHf3zcQrdQ9ovWELJ67E3K_f_eWtkWWvduT34o",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <meta name="google-site-verification" content="5NodRWHf3zcQrdQ9ovWELJ67E3K_f_eWtkWWvduT34o" />
-      </head>
       <body className={quickSand.className}>{children}</body>
     </html>
   );
